501a727a330334d480d4adecc39a7beba5f2a9d2
swap-mta <96swapnilsharma@gmail.com>
2017-08-22 22:41:17 -0700

    splashchange

--- app/src/main/java/com/mta/genseva/SplashActivity.java
+++ app/src/main/java/com/mta/genseva/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.mta.genseva;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+        builder.setTitle("Select Language")
+                .setItems(new String[]{"English", "Hindi"}, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The 'which' argument contains the index position
+                        // of the selected item
+
+                    }
+                });
+        return builder.create();
     }
 }
